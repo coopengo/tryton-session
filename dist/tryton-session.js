1171764e6af82f6e0742ad64d27b00eb3b030f65
Ali Kefia <ali.kefia@coopengo.com>
2016-09-26 10:39:24 +0200

    remove tryton-base

--- dist/tryton-session.js
+++ dist/tryton-session.js
@@ -45,9 +45,7 @@ var Session =
 /* 0 */
 /***/ function(module, exports, __webpack_require__) {
 
-	var _ = __webpack_require__(1);
-	var moment = __webpack_require__(2);
-	__webpack_require__(3)(_, moment);
+	__webpack_require__(1);
 	var Session = __webpack_require__(4);
 	__webpack_require__(23);
 	module.exports = Session;
@@ -55,146 +53,145 @@ var Session =
 
 /***/ },
 /* 1 */
-/***/ function(module, exports) {
+/***/ function(module, exports, __webpack_require__) {
 
-	module.exports = _;
+	var _ = __webpack_require__(2);
+	var moment = __webpack_require__(3);
+	var isEqual = _.isEqual;
+	_.mixin({
+	  'inherit': function (child, base) {
+	    child.prototype = _.create(base.prototype, _.assign({
+	      'constructor': child,
+	      '__': base.prototype,
+	    }, child.prototype));
+	    return child;
+	  },
+	  'assert': function (cond, msg) {
+	    if (!cond) {
+	      throw new Error(msg || 'assertion failed');
+	    }
+	  },
+	  'raise': function (msg) {
+	    throw new Error(msg);
+	  },
+	  'notImplem': function () {
+	    _.raise('not implemented');
+	  },
+	  'isPromise': function (o) {
+	    return o instanceof Promise;
+	  },
+	  'isEqual': function (a, b) {
+	    var am = a instanceof moment;
+	    var bm = b instanceof moment;
+	    if (am || bm) {
+	      if (am && bm) {
+	        return a.isSame(b);
+	      }
+	      else {
+	        return false;
+	      }
+	    }
+	    else {
+	      return isEqual(a, b);
+	    }
+	  },
+	  'objFilter': function (input, test, context) {
+	    test = test || _.identity;
+	    return _.reduce(input, (obj, v, k) => {
+	      if (test.call(context, v, k, input)) {
+	        obj[k] = v;
+	      }
+	      return obj;
+	    }, {}, context);
+	  },
+	  'decimal': function (n) {
+	    return new Number(n);
+	  },
+	  'date': function (y, M, D) {
+	    // nil values are replaced by current
+	    var values = {
+	      y: y,
+	      M: M,
+	      D: D,
+	      h: 0,
+	      m: 0,
+	      s: 0,
+	      ms: 0
+	    };
+	    values = _.objFilter(values, (v) => !(_.isUndefined(v) || v === null));
+	    var r = moment();
+	    r.set(values);
+	    r.isDate = true;
+	    return r;
+	  },
+	  'time': function (h, m, s, ms) {
+	    // nil values are replaced by current
+	    var values = {
+	      y: 0,
+	      M: 0,
+	      D: 1,
+	      h: h,
+	      m: m,
+	      s: s,
+	      ms: ms
+	    };
+	    var r = moment();
+	    r.set(_.objFilter(values, (v) => !(_.isUndefined(v) || v === null)));
+	    r.isTime = true;
+	    return r;
+	  },
+	  'datetime': function (y, M, D, h, m, s, ms, utc) {
+	    // nil values are replaced by current
+	    var values = {
+	      y: y,
+	      M: M,
+	      D: D,
+	      h: h,
+	      m: m,
+	      s: s,
+	      ms: ms
+	    };
+	    var cls = utc && moment.utc || moment;
+	    var r = cls();
+	    r.set(_.objFilter(values, (v) => !(_.isUndefined(v) || v === null)));
+	    r.isDateTime = true;
+	    return r.local();
+	  },
+	  'timedelta': function (y, M, d, h, m, s, ms) {
+	    var values = {
+	      y: y,
+	      M: M,
+	      d: d,
+	      h: h,
+	      m: m,
+	      s: s,
+	      ms: ms
+	    };
+	    var r = moment.duration(_.objFilter(values, (v) => !(_.isUndefined(v) ||
+	      v === null)));
+	    r.isTimeDelta = true;
+	    return r;
+	  }
+	});
+
 
 /***/ },
 /* 2 */
 /***/ function(module, exports) {
 
-	module.exports = moment;
+	module.exports = _;
 
 /***/ },
 /* 3 */
 /***/ function(module, exports) {
 
-	module.exports = function (_, moment) {
-	  if (_.inherit) {
-	    return;
-	  }
-	  var isEqual = _.isEqual;
-	  _.mixin({
-	    'inherit': function (child, base) {
-	      child.prototype = _.create(base.prototype, _.assign({
-	        'constructor': child,
-	        '__': base.prototype,
-	      }, child.prototype));
-	      return child;
-	    },
-	    'assert': function (cond, msg) {
-	      if (!cond) {
-	        throw new Error(msg || 'assertion failed');
-	      }
-	    },
-	    'raise': function (msg) {
-	      throw new Error(msg);
-	    },
-	    'notImplem': function () {
-	      _.raise('not implemented');
-	    },
-	    'isPromise': function (o) {
-	      return o instanceof Promise;
-	    },
-	    'isEqual': function (a, b) {
-	      var am = a instanceof moment;
-	      var bm = b instanceof moment;
-	      if (am || bm) {
-	        if (am && bm) {
-	          return a.isSame(b);
-	        }
-	        else {
-	          return false;
-	        }
-	      }
-	      else {
-	        return isEqual(a, b);
-	      }
-	    },
-	    'objFilter': function (input, test, context) {
-	      test = test || _.identity;
-	      return _.reduce(input, (obj, v, k) => {
-	        if (test.call(context, v, k, input)) {
-	          obj[k] = v;
-	        }
-	        return obj;
-	      }, {}, context);
-	    },
-	    'date': function (y, M, D) {
-	      // nil values are replaced by current
-	      var values = {
-	        y: y,
-	        M: M,
-	        D: D,
-	        h: 0,
-	        m: 0,
-	        s: 0,
-	        ms: 0
-	      };
-	      values = _.objFilter(values, (v) => !(_.isUndefined(v) || v ===
-	        null));
-	      var r = moment();
-	      r.set(values);
-	      r.isDate = true;
-	      return r;
-	    },
-	    'time': function (h, m, s, ms) {
-	      // nil values are replaced by current
-	      var values = {
-	        y: 0,
-	        M: 0,
-	        D: 1,
-	        h: h,
-	        m: m,
-	        s: s,
-	        ms: ms
-	      };
-	      var r = moment();
-	      r.set(_.objFilter(values, (v) => !(_.isUndefined(v) || v === null)));
-	      r.isTime = true;
-	      return r;
-	    },
-	    'datetime': function (y, M, D, h, m, s, ms, utc) {
-	      // nil values are replaced by current
-	      var values = {
-	        y: y,
-	        M: M,
-	        D: D,
-	        h: h,
-	        m: m,
-	        s: s,
-	        ms: ms
-	      };
-	      var cls = utc && moment.utc || moment;
-	      var r = cls();
-	      r.set(_.objFilter(values, (v) => !(_.isUndefined(v) || v === null)));
-	      r.isDateTime = true;
-	      return r.local();
-	    },
-	    'timedelta': function (y, M, d, h, m, s, ms) {
-	      var values = {
-	        y: y,
-	        M: M,
-	        d: d,
-	        h: h,
-	        m: m,
-	        s: s,
-	        ms: ms
-	      };
-	      var r = moment.duration(_.objFilter(values, (v) => !(_.isUndefined(
-	        v) || v === null)));
-	      r.isTimeDelta = true;
-	      return r;
-	    }
-	  });
-	};
-
+	module.exports = moment;
 
 /***/ },
 /* 4 */
 /***/ function(module, exports, __webpack_require__) {
 
-	var _ = __webpack_require__(1);
+	var _ = __webpack_require__(2);
 	var EventEmitter = __webpack_require__(5);
 	var rpc = __webpack_require__(6);
 	var methods = __webpack_require__(22);
@@ -668,7 +665,7 @@ var Session =
 /* 6 */
 /***/ function(module, exports, __webpack_require__) {
 
-	var _ = __webpack_require__(1);
+	var _ = __webpack_require__(2);
 	var btoa = __webpack_require__(7);
 	var request = __webpack_require__(12);
 	var json = __webpack_require__(17);
@@ -707,7 +704,7 @@ var Session =
 	    req.end((err, res) => {
 	      debug(method + ': ' + (new Date() - t) + 'ms');
 	      if (err) {
-	        debug(method + ': ko <= ' + JSON.stringify(err));
+	        debug(method + ': ko <= node error');
 	        reject(err);
 	      }
 	      else {
@@ -718,7 +715,7 @@ var Session =
 	          reject('no body from server');
 	        }
 	        else if (body.error) {
-	          debug(method + ': ko <= ' + JSON.stringify(body.error));
+	          debug(method + ': ko <= ' + body.error[0]);
 	          reject(body.error);
 	        }
 	        else {
@@ -4360,7 +4357,7 @@ var Session =
 /* 17 */
 /***/ function(module, exports, __webpack_require__) {
 
-	var _ = __webpack_require__(1);
+	var _ = __webpack_require__(2);
 	var atob = __webpack_require__(18);
 	var btoa = __webpack_require__(7);
 
